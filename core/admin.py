a104ebd21e806e9fcdb2f55e9b971c01e8bb84bc
MichaelTaylor96 <michael96taylor@gmail.com>
2019-04-01 18:45:34 -0400

    Setup admin and ready for heroku deployment

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Card, Deck, Category
 
 # Register your models here.
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Deck)
+class DeckAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
