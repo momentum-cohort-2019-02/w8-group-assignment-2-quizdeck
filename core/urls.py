26519da3b083257509e25e37bf4214085ece6f90
Ryan Catrine <ryancatrine@gmail.com>
2019-04-01 17:30:08 -0400

    Completed initial urls.py pages.

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.db import models
+from django.conf.urls import url
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('')
+]
--- quiz_deck/urls.py
+++ quiz_deck/urls.py
@@ -16,10 +16,19 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('registration.backends.simple.urls')),
+]
+
+urlpatterns += [
+    path('core/', include('core.urls')),
+    path('', RedirectView.as_view(url='/core/', permanent=True)),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
 
 if settings.DEBUG:
