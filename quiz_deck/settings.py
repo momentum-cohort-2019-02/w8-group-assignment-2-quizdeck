f82a9f6fe2a55b1a2908871fab08ab9354625070
AJ Bryce <46729511+AlanaKaimi@users.noreply.github.com>
2019-04-01 13:48:29 -0400

    specified database and user

--- quiz_deck/settings.py
+++ quiz_deck/settings.py
@@ -77,9 +77,8 @@ WSGI_APPLICATION = 'quiz_deck.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'NAME': 'quiz_deck',
+        'USER': 'quiz_deck',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
