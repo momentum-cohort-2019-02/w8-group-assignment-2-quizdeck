05b6756c6d0600b5bafaea0a160edbf01e7b8383
AlanaKaimi <ajbryce@live.com>
2019-04-01 17:46:31 -0400

    Basic Card model, includes Question & Answer TextFields.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Card(models.Model):
+    """Model representing a Card."""
+
+    question = models.TextField(max_length=200)
+    answer = models.TextField(max_length=250)
+    
