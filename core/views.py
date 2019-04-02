d5992cf8dc753db7660c0ca2a828cc87c6f1f7e4
Ryan Catrine <ryancatrine@gmail.com>
2019-04-02 13:44:39 -0400

    Started work on navbar

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+# from core.models import Create, Play
+
 
 # Create your views here.
+def index(request):
+    """View function for home page of site."""
+    cards = Card.objects.all()
+    # Render the HTML template index.html with the data in the context variable
+    response = render(request, 'index.html', {
+        "cards": cards,
+    })
+    return response
