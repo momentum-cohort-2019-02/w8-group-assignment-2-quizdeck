b08c11b8141b819d38a87b4b39f4b8fa99154d0e
Ryan Catrine <ryancatrine@gmail.com>
2019-04-03 16:54:34 -0400

    Set up deck detail pages.

--- core/models.py
+++ core/models.py
@@ -26,12 +26,14 @@ class Card(models.Model):
 class Deck(models.Model):
     """Model representing a Deck of cards."""
 
+    #Need to make number of cards auto populate?
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=255, unique=True)
-    # difficulty = models.CharField(max_length=100)
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # date_added = models.DateTimeField(auto_now_add=True)
-    # description = models.TextField(max_length=1000, default='N/A')
+    card_count = models.CharField(max_length=100, null=True)
+    difficulty = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
+    description = models.TextField(max_length=1000, default='N/A', null=True)
 
     
     def __str__(self):
