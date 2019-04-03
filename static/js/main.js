6425f095947efef1429cdfbf5ee35e8e3839f254
AlanaKaimi <ajbryce@live.com>
2019-04-03 14:12:05 -0400

    tested to make sure changes from main.js ended up in bundle.js when `npm run watch` was running. Works!

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,14 @@
 const Cookies = require('js-cookie')
 const deepmerge = require('deepmerge')
 
+function qS (selector) {
+  return document.querySelector(selector)
+}
+
+function qSA (selector) {
+  return document.querySelectorAll(selector)
+}
+
 // This function will be helpful if we need to fetch anything from an API, for example, fetching trivia questions from this API: https://opentdb.com/api_config.php
 function request (url, options) {
   if (!options) {
