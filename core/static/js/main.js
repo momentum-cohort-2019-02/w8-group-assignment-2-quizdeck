e249765f7c87e7602f56eef00d93da9e3f9afdb8
AlanaKaimi <ajbryce@live.com>
2019-04-04 12:45:56 -0400

    checking out card css

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -21,3 +21,49 @@ function request (url, options) {
 
   return fetch(url, deepmerge(defaultOptions, options))
 }
+
+function createCard (input) {
+  // finds the div id="create-card" on the create page
+  const createCardDiv = qs('#create-card')
+  // & sets it to empty
+  createCardDiv.innerHTML = ''
+
+  const card = document.createElement('div')
+
+
+
+  const question = document.createElement('form')
+
+}
+
+// document.addEventListener('DOMContentLoaded', function () {
+  
+//   const createCardForm = qs('#create_card')
+//   createCardForm.addEventListener('submit', function (event) {
+//     event.preventDefault()
+
+//     const cardField = qs('#task-field')
+//     const body = {
+//       'task': taskField.value
+//     }
+//     taskField.value = ''
+
+//     request(newTaskForm.action, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(body)
+//     })
+//       .then(response => {
+//         if (!response.ok) {
+//           throw Error(response.statusText)
+//         }
+//         return response.text()
+//       })
+//       .then(text => {
+//         const taskFragment = htmlToNodes(text)
+//         q('#task-list').appendChild(taskFragment)
+//       })
+//   })
+// })
