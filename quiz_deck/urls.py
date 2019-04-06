9ad759b47e3d4368b912cfadc8ccb6f51ef83204
AlanaKaimi <ajbryce@live.com>
2019-04-06 11:47:16 -0400

    reinstated empty url path to go to core

--- quiz_deck/urls.py
+++ quiz_deck/urls.py
@@ -23,8 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('core/', include('core.urls')),
-    path('accounts/', include('registration.backends.simple.urls'))
-#     path('', RedirectView.as_view(url='/core/', permanent=True))
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('', RedirectView.as_view(url='/core/', permanent=True)),
 ] 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
